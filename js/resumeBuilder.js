bad2bf2702f43a57411fc85d772eb12b1cc365c7
Pedro Duarte <pedrohlduarte@gmail.com>
2017-07-30 21:30:58 -0300

    segundo exercicio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,11 @@
 /*
 Vamos começar a editar o cabeçalho:
  */
-$("#header").append("Pedro Duarte");
-/*Procuramos um elemento com id = header nesse codigo do JQuery*/
+$("#main").append("Pedro Duarte");
+/*Procuramos um elemento com id = main nesse codigo do JQuery*/
+
+/*ADICIONAR NO CABEÇALHO O NOME USANDO TROCA DE INFORMAÇÃO*/
+var nome = "Pedro Duarte";
+var formatoNome = HTMLheaderName.replace("%data%", nome);
+
+$("#header").append(formatoNome);
