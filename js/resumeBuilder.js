d41b1eabab37ab35b037af1c38d5c2a1d996eb8d
Pedro Duarte <pedrohlduarte@gmail.com>
2017-07-31 14:29:30 -0300

    variaveis ok

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -21,59 +21,59 @@ $("#header").prepend(formatoNome);
 
 /*ADICIONANDO OBJETO NA MINHA BIO*/
 var bio = {
-	"name" : "Pedro Duarte",
-	"papel" : "Desenvolvedor Front-End",
-	"contatos" : {
-		"mobile" : "11972436305",
-		"email" : "pedro.duarte@letscode-academy.com",
-		"github" : "pedro",
-		"twitter" : "@pedro",
-		"location" : "San Francisco"
+    "name": "Pedro Duarte",
+    "papel": "Desenvolvedor Front-End",
+    "contatos": {
+       "mobile": "11972436305",
+       "email": "pedro.duarte@letscode-academy.com",
+       "github": "pedro",
+       "twitter": "@pedro",
+       "location": "San Francisco"
 	},
-	"welcomeMessage" : "Oiiiii!!",
-	"skills" : ["programador", "Front-End", "wordpress"],
-	"bioPic" : "images/eu.jpg"
+	"welcomeMessage": "Oiiiii!!",
+	"skills": ["programador", "Front-End", "wordpress"],
+	"bioPic": "images/eu.jpg"
 }
 
 var education = {
 	"schools": [
 		{
-		"name": "Universidade Federal de Ouro Preto",
-		"city" : "Ouro Preto",
-		"dates" : 2014,
-		"url" : "http://www.fideitabira.com.br"
+		   "name": "Universidade Federal de Ouro Preto",
+		   "city" : "Ouro Preto",
+		   "dates": 2014,
+		   "url": "http://www.fideitabira.com.br"
 		},
 		{
-		"name": "FIDE",
-		"city" : "Itabira",
-		"dates" : 2006,
-		"url" : "http://www.fideitabira.com.br"
+		   "name": "FIDE",
+		   "city": "Itabira",
+		   "dates": 2006,
+		   "url": "http://www.fideitabira.com.br"
 		}
-	]
-	"Courses" : [
+	],
+	"Courses": [
 		{
-			"title" : "HTML/CSS e JS",
-			"school" : "Let's Code",
-			"date" : 2017,
-			"url" : "http://letscode-academy.com/"
+			"title": "HTML/CSS e JS",
+			"school": "Let's Code",
+			"date": 2017,
+			"url": "http://letscode-academy.com/"
 		}
 	]
 }
 
 var work = {
-	"jobs" : [
+	"jobs": [
 		{
-			"employer" : "Let's Code",
-			"title" : "Desenvolvedor Front-End",
-			"date" : "Março 2017 - atual",
-			"description" : "Descrição"
+			"employer": "Let's Code",
+			"title": "Desenvolvedor Front-End",
+			"date": "Março 2017 - atual",
+			"description": "Descrição"
 		},
 		{
-			"employer" : "PDWebDesign",
-			"title" : "Desenvolvedor Front-End",
-			"date" : "Janeiro 2008 - atual",
-			"description" : "Descrição"
-		},
+			"employer": "PDWebDesign",
+			"title": "Desenvolvedor Front-End",
+			"date": "Janeiro 20 - atual",
+			"description": "Descrição"
+		}
 	]
 }
 
