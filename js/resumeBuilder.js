daa42a3e65ae1efa4dafd7bfdea04547bee7c073
Pedro Duarte <pedrohlduarte@gmail.com>
2017-08-01 18:45:48 -0300

    lesson5 parte 5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -89,3 +89,14 @@ var work = {
 }
 
 
+/*EXERCICIO COLOCAR PARTE DE WORK*/
+for (job in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+}
